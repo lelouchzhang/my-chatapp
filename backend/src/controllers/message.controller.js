4d90a70277d7e414410704e56b7e3b5ff584477e
lelouchZhang <zhch8128@gmail.com>
2026-03-25 02:00:22 +0800

    feat: 实现消息功能，包括私信发送、获取侧边栏用户和获取私信历史

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,61 @@
+import Message from "../models/messages.model.js";
+import User from "../models/users.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const SidebarUsers = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    const users = await User.find({ _id: { $ne: currentUser._id } }).select(
+      "-password"
+    );
+    res.status(200).json(users);
+  } catch (error) {
+    console.log("Error in SidebarUsers controller:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getDirectMessages = async (req, res) => {
+  try {
+    const { userId: dmUserId } = req.params;
+    const senderId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: senderId, receiverId: dmUserId },
+        { senderId: dmUserId, receiverId: senderId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getDirectMessages controller:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const sendDirectMessage = async (req, res) => {
+  try {
+    const { userId: dmUserId } = req.params;
+    const senderId = req.user._id;
+    const { text, image } = req.body;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = newMessage({
+      senderId,
+      receiverId: dmUserId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    // todo: realtime message using socket.io(ws) here.
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendDirectMessage controller:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import AuthRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import AuthRoutes from "./routes/auth.route.js";
+import MessageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.get("/", () => {
 });
 
 app.use("/api/auth", AuthRoutes);
+app.use("/api/message", MessageRoutes);
 
 app.listen(PORT, () => {
   connectDB();
